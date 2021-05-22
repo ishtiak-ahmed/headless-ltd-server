954720cdc9410e90ff42bf957457d059633cd480
Ishtiak Ahmed <iahmed1606@gmail.com>
2021-05-22 16:02:55 +0600

    delete folder method done

--- app.js
+++ app.js
@@ -10,21 +10,6 @@ const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config()
 const ObjectId = require('mongodb').ObjectId;
 
-// const data = require('data')
-const data = [
-  { _id: "MongoDB", children: [] },
-  { _id: "dbm", children: [] },
-  { _id: "Databases", children: ["MongoDB", "dbm"] },
-  { _id: "Languages", children: [] },
-  { _id: "Programming", children: ["Databases", "Languages"] },
-  { _id: "Books", children: ["Programming"] }
-];
-// console.log(data)
-
-app.get('/', (req, res) => {
-  res.send(`<h1>This is for headless server!</h1>`)
-})
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xzynl.mongodb.net/${process.env.DATABASE}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
@@ -42,15 +27,34 @@ client.connect(err => {
   // Delete Folder
   app.delete('/delete/:id', (req, res) => {
 
-    // folderCollection.deleteOne({ _id: req.params.id }, (err, docs) => {
-    //   res.send('item deleted')
-    // })
     folderCollection.deleteOne({ _id: req.params.id })
       .then(result => {
         res.send(result.deletedCount > 0);
       })
   })
 
+  // AddFolder
+  app.post('/addfolder/:parentId', (req, res) => {
+    console.log(req.body)
+    console.log(req.params.parentId)
+    const id = `${req.params.parentId}-${req.body.name}`
+    folderCollection.insertOne({ _id: id, name: req.body.name, children: [] })
+      .then(result => {
+        res.send(result.insertedCount > 0);
+      })
+  })
+
+  // Update Parent Children
+  app.patch('/updateparent/:id', (req, res) => {
+    const childs = req.body.childs
+    folderCollection.updateOne({ _id: req.params.id },
+      {
+        $set: { 'children': childs }
+      }
+    ).then(result => {
+      res.send(result.modifiedCount > 0)
+    })
+  })
 });
 
 app.listen(process.env.PORT || port)
